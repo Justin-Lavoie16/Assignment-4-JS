0c734b037a99f05a6aaf78f35728b5ad5a3bb2b1
Justin-Lavoie16 <Justin-Lavoie16@users.noreply.github.com>
2022-11-30 14:24:48 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,39 +4,32 @@ if (navigator.serviceWorker) {
   })
 }
 
-"use strict"
+;("use strict")
 
 function myButtonClicked() {
- 
-  const age = document.getElementById("age").value;
-  const day = document.getElementById("day").value;
-  
- if((age == "small") && (day == "no")) {
-     document.getElementById("answers").innerHTML =
-      ("You are buying a small poutine with no cheese");
-    
-} else if((age == "small") && (day == "yes")) {
-    document.getElementById("answers").innerHTML =
-      ("You are buying a small poutine with cheese");
+  const age = document.getElementById("age").value
+  const day = document.getElementById("day").value
 
- } else if((age == "medium") && (day == "no")) {
+  if (age == "small" && day == "no") {
     document.getElementById("answers").innerHTML =
-      ("You are buying a medium poutine with no cheese.");
-
- } else if((age == "medium") && (day == "yes")) {
+      "You are buying a small poutine with no cheese"
+  } else if (age == "small" && day == "yes") {
     document.getElementById("answers").innerHTML =
-      ("You are buying a medium poutine with cheese");
-
- } else if((age == "big") && (day == "no")) {
+      "You are buying a small poutine with cheese"
+  } else if (age == "medium" && day == "no") {
     document.getElementById("answers").innerHTML =
-      ("You are buying a large poutine with no cheese");
-
- } else if((age == "big") && (day == "yes")) {
+      "You are buying a medium poutine with no cheese."
+  } else if (age == "medium" && day == "yes") {
     document.getElementById("answers").innerHTML =
-      ("You are buying a large poutine with cheese");
-
- } else {
+      "You are buying a medium poutine with cheese"
+  } else if (age == "big" && day == "no") {
     document.getElementById("answers").innerHTML =
-      ("Uh, are you going to get something??");
-}
+      "You are buying a large poutine with no cheese"
+  } else if (age == "big" && day == "yes") {
+    document.getElementById("answers").innerHTML =
+      "You are buying a large poutine with cheese"
+  } else {
+    document.getElementById("answers").innerHTML =
+      "Uh, are you going to get something??"
+  }
 }
